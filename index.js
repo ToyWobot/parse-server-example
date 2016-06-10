12d26b91d8967388ceaa29128867ea2942aa57a6
Ritesh <Ritesh@Riteshs-MacBook-Air.local>
2016-06-10 11:24:05 +0545

    added dashboard

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var ParseDashboard = require('parse-dashboard');
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -21,6 +22,19 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 });
+
+var dashboard = new ParseDashboard({
+    "apps": [
+        {
+            "serverURL": "https://tw-parse-server.herokuapp.com/parse",
+            "appId": "myAppId",
+            "masterKey": "myMasterKey",
+            "appName": "EduApp"
+        }
+    ]
+});
+
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
@@ -33,6 +47,9 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
+
+app.use('/dashboard', dashboard);
+
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
