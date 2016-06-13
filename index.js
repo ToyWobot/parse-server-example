eec070f955513737bd96c74587aff3dffce3eb69
Ritesh <Ritesh@Riteshs-MacBook-Air.local>
2016-06-13 09:49:52 +0545

    placed certificate at different place

--- index.js
+++ index.js
@@ -35,9 +35,12 @@ var pmApi = new ParseServer({
         classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
     },
     push :{
-        
+        android: {
+            senderId: '', // The Sender ID of GCM
+            apiKey: '' // The Server API Key of GCM
+        },
         ios:{
-            pfx: __dirname + '/cloud/pm/key/Certificates_PM_EDU_Production.p12',
+            pfx: __dirname + '/certs/Certificates_PM_EDU_Production.p12',
             bundleId: 'com.edu.playmessenger',
             production: true
         }
