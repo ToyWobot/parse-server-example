b14045678a41176dc1a15cd4d074165594e2a4df
Ritesh <Ritesh@Riteshs-MacBook-Air.local>
2016-06-14 21:24:36 +0545

    adding server through json file

--- index.js
+++ index.js
@@ -13,94 +13,101 @@ if (!eduDatabaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var eduApi = new ParseServer({
-  databaseURI: eduDatabaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/edu/main.js',
-  appId: process.env.EDU_APP_ID,
-  masterKey: process.env.EDU_MASTER_KEY, //Add your master key here. Keep it secret!
-  restAPIKey: process.env.EDU_REST_API_KEY,
-  clientKey: process.env.EDU_CLIENT_KEY,
-  serverURL: (process.env.SERVER_URL + '/edu'),  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
-  push :{
-        android : {
-            senderId: '316172193173',
-            apiKey: 'AIzaSyB3SuPcXiEUvZCT284ctWJWG3pIQQyT9JE'
-        },
-        ios:[
-            {
-                pfx: __dirname + '/certs/eddy/Certificates_Eddy_APNS_Development.p12',
-                bundleId: 'com.eddy.app',
-                production: false
-            },
-            {
-                pfx: __dirname + '/certs/eddy/Certificates_Eddy_APNS_Production.p12',
-                bundleId: 'com.eddy.app',
-                production: true
-            }
-        ]
-    }
+//
+//var eduApi = new ParseServer({
+//  databaseURI: eduDatabaseUri || 'mongodb://localhost:27017/dev',
+//  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/edu/main.js',
+//  appId: process.env.EDU_APP_ID,
+//  masterKey: process.env.EDU_MASTER_KEY, //Add your master key here. Keep it secret!
+//  restAPIKey: process.env.EDU_REST_API_KEY,
+//  clientKey: process.env.EDU_CLIENT_KEY,
+//  serverURL: (process.env.SERVER_URL + '/edu'),  // Don't forget to change to https if needed
+//  liveQuery: {
+//    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+//  },
+//  push :{
+//        android : {
+//            senderId: '316172193173',
+//            apiKey: 'AIzaSyB3SuPcXiEUvZCT284ctWJWG3pIQQyT9JE'
+//        },
+//        ios:[
+//            {
+//                pfx: __dirname + '/certs/eddy/Certificates_Eddy_APNS_Development.p12',
+//                bundleId: 'com.eddy.app',
+//                production: false
+//            },
+//            {
+//                pfx: __dirname + '/certs/eddy/Certificates_Eddy_APNS_Production.p12',
+//                bundleId: 'com.eddy.app',
+//                production: true
+//            }
+//        ]
+//    }
+//});
+//
+//
+//var alyApi = new ParseServer({
+//    databaseURI: alyDatabaseUri || 'mongodb://localhost:27017/dev',
+//    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+//    appId: '3763213300',
+//    masterKey: 'BStgFP6PCX3MCTGYljUsHn7EUCeRu74u', //Add your master key here. Keep it secret!
+//    restAPIKey: 'REsA64SL5FIBIeZthgBDYWwY7daQpAm1',
+//    clientKey: 'RN2hNqvuelw4NyRNxrN3ELPPyAnD5cdk',
+//    serverURL: (process.env.SERVER_URL + '/aly'),  // Don't forget to change to https if needed
+//    liveQuery: {
+//        classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+//    }
+//});
+//
+//// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+//// If you wish you require them, you can set them as options in the initialization above:
+//// javascriptKey, restAPIKey, dotNetKey, clientKey
+//
+//var app = express();
+//
+//// Serve static assets from the /public folder
+//app.use('/public', express.static(path.join(__dirname, '/public')));
+//app.use('/parse/edu', eduApi);
+//app.use('/parse/aly', alyApi);
+//
+//
+//
+//var dashboardConfig ={
+//    "allowInsecureHTTP": process.env.DASHBOARD_INSECURE_HTTP,
+//    "apps": [
+//        {
+//            "serverURL": "https://tw-parse-server.herokuapp.com/parse/edu",
+//            "appId": "3763213299",
+//            "masterKey": "9jwczspNPUAbasjGSs0TcUklsxnbLzeG",
+//            "appName": "EduApp",
+//            "production": false
+//        },
+//        {
+//            "serverURL": "https://tw-parse-server.herokuapp.com/parse/aly",
+//            "appId": "3763213300",
+//            "masterKey": "BStgFP6PCX3MCTGYljUsHn7EUCeRu74u",
+//            "appName": "AlyApp",
+//            "production": false
+//        }
+//    ],
+//    "users":[
+//        {
+//            "user":"admin",
+//            "pass":"pass"
+//        }
+//
+//    ]
+//};
+
+var serverConfig = require('server-config.json');
+
+serverConfig.servers.keys(apps).forEach(function(appId) {
+    var appOptions = apps[appId];
+    var appObject = new ParseServer(appOptions);
+    app.use('/parse/'+appId, appObject);
 });
 
-
-var alyApi = new ParseServer({
-    databaseURI: alyDatabaseUri || 'mongodb://localhost:27017/dev',
-    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-    appId: '3763213300',
-    masterKey: 'BStgFP6PCX3MCTGYljUsHn7EUCeRu74u', //Add your master key here. Keep it secret!
-    restAPIKey: 'REsA64SL5FIBIeZthgBDYWwY7daQpAm1',
-    clientKey: 'RN2hNqvuelw4NyRNxrN3ELPPyAnD5cdk',
-    serverURL: (process.env.SERVER_URL + '/aly'),  // Don't forget to change to https if needed
-    liveQuery: {
-        classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-    }
-});
-
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
-
-var app = express();
-
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
-app.use('/parse/edu', eduApi);
-app.use('/parse/aly', alyApi);
-
-
-
-var dashboardConfig ={
-    "allowInsecureHTTP": process.env.DASHBOARD_INSECURE_HTTP,
-    "apps": [
-        {
-            "serverURL": "https://tw-parse-server.herokuapp.com/parse/edu",
-            "appId": "3763213299",
-            "masterKey": "9jwczspNPUAbasjGSs0TcUklsxnbLzeG",
-            "appName": "EduApp",
-            "production": false
-        },
-        {
-            "serverURL": "https://tw-parse-server.herokuapp.com/parse/aly",
-            "appId": "3763213300",
-            "masterKey": "BStgFP6PCX3MCTGYljUsHn7EUCeRu74u",
-            "appName": "AlyApp",
-            "production": false
-        }
-    ],
-    "users":[
-        {
-            "user":"admin",
-            "pass":"pass"
-        }
-
-    ]
-};
-
-var dashboard = new ParseDashboard(dashboardConfig, dashboardConfig.allowInsecureHTTP);
-
-
+var dashboard = new ParseDashboard(serverConfig.dashboard, serverConfig.dashboard.allowInsecureHTTP);
 app.use('/dashboard', dashboard);
 
 
